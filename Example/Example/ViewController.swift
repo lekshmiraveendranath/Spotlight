34aae878fa9f0feed3d0e8c4c6dbf5d0df85e78e
Lekshmi Raveendranathapanicker <lekshmi.ravindranath@gmail.com>
2018-02-24 17:07:18 -0800

    Fixes sample project for different screen sizes

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -22,10 +22,10 @@ class ViewController: UIViewController {
         guard let rightBB1 = navigationItem.rightBarButtonItem, let leftBB1 = navigationItem.leftBarButtonItem else { return }
 
         let n1 = SpotlightNode(text: "Show spotlight on a bar button item", target: .barButton(rightBB1))
-        let n2 = SpotlightNode(text: "Moved spotlight to the title using a point and radius", target: .point(CGPoint(x: view.bounds.width / 2.0, y: 44), radius: 25))
+        let n2 = SpotlightNode(text: "Moved spotlight to the title using a point and radius", target: .point(CGPoint(x: view.bounds.width / 2.0, y: 44), radius: 50))
         let n3 = SpotlightNode(text: "Moved the spotlight again", target: .barButton(leftBB1))
         let n4 = SpotlightNode(text: "Can higlight individual views, and this view can move around to prevent intersection with spotlight", target: .view(verticalView))
-        let n5 = SpotlightNode(text: "", target: .point(CGPoint(x: view.bounds.width / 2.0, y: 214), radius: 116))
+        let n5 = SpotlightNode(text: "", target: .point(CGPoint(x: view.bounds.width / 2.0, y: spotlightLabel.frame.midY), radius: 116))
 
          let nodes = [n1, n2, n3, n4, n5]
          Spotlight().startIntro(from: self, withNodes: nodes)
