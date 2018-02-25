7b5362eac3adf2f92c9140dd0c86759e78c0d487
Lekshmi Raveendranathapanicker <lekshmi.ravindranath@gmail.com>
2018-02-24 23:55:32 -0800

    Sample project tweaks

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -16,18 +16,20 @@ class ViewController: UIViewController {
     
     @IBAction func startTutorial() {
 
-        Spotlight.delay = 4.0
-        Spotlight.showInfoBackground = false
+        Spotlight.delay = 5.0
 
-        guard let rightBB1 = navigationItem.rightBarButtonItem, let leftBB1 = navigationItem.leftBarButtonItem else { return }
+        guard let rightBB1 = navigationItem.rightBarButtonItem,
+            let leftBB1 = navigationItem.leftBarButtonItem,
+            let navBar = navigationController?.navigationBar else { return }
 
-        let n1 = SpotlightNode(text: "Show spotlight on a bar button item", target: .barButton(rightBB1))
-        let n2 = SpotlightNode(text: "Moved spotlight to the title using a point and radius", target: .point(CGPoint(x: view.bounds.width / 2.0, y: 44), radius: 50))
-        let n3 = SpotlightNode(text: "Moved the spotlight again", target: .barButton(leftBB1))
-        let n4 = SpotlightNode(text: "Can higlight individual views, and this view can move around to prevent intersection with spotlight", target: .view(verticalView))
-        let n5 = SpotlightNode(text: "", target: .point(CGPoint(x: view.bounds.width / 2.0, y: spotlightLabel.frame.midY), radius: 116))
+        let n0 = SpotlightNode(text: "Sit back and let Spotlight auto advance, or tap anywhere on screen to advance manually", target: .point(.zero, radius: 0))
+        let n1 = SpotlightNode(text: "Show spotlight on a bar button item", target: .barButton(leftBB1))
+        let n2 = SpotlightNode(text: "Moved spotlight to the title using a point and radius", target: .point(CGPoint(x: view.bounds.width / 2.0, y: navBar.frame.midY), radius: 30))
+        let n3 = SpotlightNode(text: "Moved the spotlight again. Targets can be views, barbuttons, tab bar items, points and more", target: .barButton(rightBB1))
+        let n4 = SpotlightNode(text: "The Info view can move around to prevent intersection with spotlight", target: .view(verticalView))
+        let n5 = SpotlightNode(text: "The End", target: .view(spotlightLabel))
 
-         let nodes = [n1, n2, n3, n4, n5]
+         let nodes = [n0, n1, n2, n3, n4, n5]
          Spotlight().startIntro(from: self, withNodes: nodes)
     }
 
